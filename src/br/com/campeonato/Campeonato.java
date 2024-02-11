ada7263768e119e40013848b0ebb09f9939759d9
Gabriel Augusto <decauser@hotmail.com>
2024-02-11 09:31:50 -0300

    Função sorteia times e primeiro set de partidas adicionadas

--- src/br/com/campeonato/Campeonato.java
+++ src/br/com/campeonato/Campeonato.java
@@ -7,8 +7,22 @@ public class Campeonato {
 	
 	private ArrayList<Clube> clubes = new ArrayList<Clube>();
 	
+	public Campeonato(ArrayList<Clube> c) {
+		this.clubes = c;
+	}
+	
 	public void jogarCampeonato() {
+		sorteiaTimes();
 		
+		int i;
+		
+		System.out.println("Primeiro Grupo");
+		
+		for(i = 1; i <= 6; i+=2) {
+			System.out.println("" + this.clubes.get(i-1).getNome() + " X " + this.clubes.get(i).getNome());
+			jogarPartida(this.clubes.get(i-1),this.clubes.get(i));
+			System.out.println();
+		}
 	}
 	
 	private void jogarPartida(Clube m, Clube v) {
@@ -21,13 +35,13 @@ public class Campeonato {
 		if(golsTime1 > golsTime2) {
 			m.ganhar(saldoGols);
 			v.perder(saldoGols);
-			System.out.println("Time " + m.getNome() + " ganhou");
+			System.out.println("" + m.getNome() + " ganhou");
 			System.out.println("Placar de " + golsTime1 + " - " + golsTime2);
 		}
 		else if(golsTime1 < golsTime2) {
 			m.perder(saldoGols);
 			v.ganhar(saldoGols);
-			System.out.println("Time " + v.getNome() + " ganhou");
+			System.out.println("" + v.getNome() + " ganhou");
 			System.out.println("Placar de " + golsTime1 + " - " + golsTime2);
 		} else {
 			m.empatar();
@@ -35,6 +49,21 @@ public class Campeonato {
 			System.out.println("Empate");
 			System.out.println("Placar de " + golsTime1 + " - " + golsTime2);
 		}
+	}
+	
+	private void sorteiaTimes() {
+		ArrayList<Clube> copia = new ArrayList<Clube>();
+		ArrayList<Clube> lista = new ArrayList<Clube>();
+		copia = this.clubes;
+		
+		Random rn = new Random();
+		
+		for(int i = copia.size()-1; i >= 0; i--) {
+			int rand = rn.nextInt(i+1);
+			lista.add(copia.get(rand));
+			copia.remove(rand);
+		}
+		this.clubes = lista;
 	}
 	
 	public ArrayList<Clube> getClassificacao() {
